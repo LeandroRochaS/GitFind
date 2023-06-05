5078c4acb09957a27e39d8f385097c2bb1d7e73e
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-06-05 10:52:57 -0300

    Deploy n

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/global.css";
-import { Home } from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About } from "./pages/About";
-import { Header } from "./components/Header";
-import { NotFound } from "./pages/404";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/global.css';
+import { Home } from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About } from './pages/About';
+import { NotFound } from './pages/404';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
-        </Routes>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
